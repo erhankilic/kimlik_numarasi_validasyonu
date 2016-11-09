4c763f6dcd6610dcc39e24e727e4d7a8eca8e2c3
Erhan Kılıç <erhan_kilic@outlook.com>
2016-11-09 13:51:06 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function validateTcId(id) {
+    if (!id || parseInt(id.charAt(1)) === 0 || id.length !== 11) {
+        return false;
+    }
+    var numbers = id.split('');
+    var i = 0;
+    var sum = 0;
+    var neg = 0;
+    var firstTen = 0;
+    while (i < 11) {
+        numbers[i] = parseInt(numbers[i]);
+        i++;
+    }
+    i = 0;
+    while (i < 9) {
+        (i % 2 === 0) ? (sum += numbers[i]) : (neg += numbers[i]);
+        firstTen += numbers[i];
+        i++;
+    }
+    firstTen += numbers[9];
+    //console.log('Firstten: ' + firstTen);
+    //console.log('(7 * sum - neg) % 10) and numbers[9]: ' + ( (7 * sum - neg) % 10) + ' ' + numbers[9]);
+    if (((7 * sum - neg) % 10) != numbers[9]) {
+        return false;
+    }
+    return (firstTen % 10) == numbers[10];
+}
